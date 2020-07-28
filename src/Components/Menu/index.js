2aaab114c615190ec27f3ed148703c012eaf6f6e
RafaelPomar <rafaelpseng@gmail.com>
2020-07-28 13:45:40 -0300

    Atualizações

--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../../assets/img/LogoMain.png';
+import Logo from '../../assets/img/cozinhaflix.png';
 import './Menu.css';
 import Button from '../Button';
 //import ButtonLink from './components/ButtonLink';
@@ -8,7 +8,7 @@ function Menu() {
     return (
         <nav className="Menu">
             <a href="/">
-            <img className="Logo" src={Logo} alt="AluraFlix Logo"/>
+            <img className="Logo" src={Logo} alt="CozinhaFlix Logo"/>
             </a>
 
             <Button as ="a" className="ButtonLink" href="/">
