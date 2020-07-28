f240cf8accb594a3164449c0a576b6131b830fb5
RafaelPomar <rafaelpseng@gmail.com>
2020-07-28 18:45:44 -0300

    Aula 2

---
+++ src/pages/cadastro/video/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CadastroVideo() {
+    return(
+        <div>
+            Página de cadatro de video
+        </div>
+    )
+}
+
+export default CadastroVideo;
---
+++ src/pages/home/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Menu from '../../Components/Menu';
+import dados from '../../data/dados_iniciais.json';
+import BannerMain from '../../Components/BannerMain';
+import Carousel from '../../Components/Carousel';
+import Footer from '../../Components/Footer';
+
+function App() {
+  return (
+    <div>
+     <Menu />
+
+    <BannerMain
+      videoTitle={dados.categorias[0].videos[0].titulo}
+      url={dados.categorias[0].videos[0].url}
+      videoDescription={dados.categorias[0].videos[0].videoDescription}
+    />
+    <Carousel category={dados.categorias[1]}/>
+    <Footer/>
+    </div>
+  );
+}
+
+export default App;
