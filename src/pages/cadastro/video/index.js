ee1c8795c46767038c0ab2df43abe42ccdd5bcfd
RafaelPomar <rafaelpseng@gmail.com>
2020-08-02 20:44:22 -0300

    Aula 5 - Finalizada

--- src/pages/cadastro/video/index.js
+++ src/pages/cadastro/video/index.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import PageDefault from '../../../Components/PageDefault';
 import FormField from '../../../Components/FormField';
 import useForm from '../../../hooks/useForm';
 import Button from '../../../Components/Button';
 import videosRepository from '../../../repositories/videos';
+import categoriasRepository from '../../../repositories/categorias';
 
 function CadastroVideo() {
   const history = useHistory();
+  const [categorias, setCategorias] = useState([]);
+  const categoryTitles = categorias.map(({ titulo }) => titulo);
   const { handleChange, values } = useForm({
     titulo: '',
     url: '',
     categoria: '',
   });
+
+  useEffect(() => {
+    categoriasRepository
+      .getAll()
+      .then((categoriasFromServer) => {
+        setCategorias(categoriasFromServer);
+      });
+  }, []);
 
   return (
     <PageDefault>
@@ -21,10 +32,12 @@ function CadastroVideo() {
       <form onSubmit={(event) => {
         event.preventDefault();
 
+        const categoriaEscolhida = categorias.find((categoria) => categoria.titulo === values.categoria);
+
         videosRepository.create({
           titulo: values.titulo,
           url: values.url,
-          categoriaId: 1,
+          categoriaId: categoriaEscolhida.id,
         })
           .then(() => { history.push('/'); });
 
@@ -50,6 +63,7 @@ function CadastroVideo() {
           name="categoria"
           value={values.categoria}
           onChange={handleChange}
+          suggestions={categoryTitles}
         />
 
         <Button type="submit">
